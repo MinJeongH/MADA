3dbe101f8b12f9fde0f6bb97a6fff3a591971810
MinJengH <gkaalswjd2@gmail.com>
2022-03-26 22:31:58 +0900

    맵 캘린더 페이지 연결

--- src/component/map.tsx
+++ src/component/map.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './pages.scss';
 
 const Map = () => {
+  const nav = useNavigate();
+
+  const goToCalender = () => {
+    nav({
+      pathname: '/calender',
+    });
+  };
   return (
     <section className='map'>
       <div className='header'>
@@ -9,7 +18,12 @@ const Map = () => {
           <input type='text' name='search' id='search' />
           <img src='/search.svg' alt='search_icon' />
         </label>
-        <img className='calender' src='/Date.svg' alt='calender_icon' />
+        <img
+          className='calender'
+          src='/Date.svg'
+          alt='calender_icon'
+          onClick={goToCalender}
+        />
       </div>
       <img className='add' src='/content_add.svg' alt='add_icon' />
     </section>
